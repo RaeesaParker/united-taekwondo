d1cb4d2cbb569ff58b6dcc2e38b00c38befd2fb7
RaeesaParker <raeesaparker101@gmail.com>
2023-10-06 15:52:40 +0100

    Add Chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
@@ -7,8 +8,28 @@ import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
+
+const theme = extendTheme({
+  colors: {
+    blue: {
+      dark: "#011627",
+      light: "#007CBE"
+    },
+		red:{
+			dark: "#F71735"
+		},
+		tones:{
+			white:"#FDFFFC",
+			darkGrey:"#C2C2C2",
+			lightgrey:"#D9D9D9"
+		}
+  },
+})
+
+
+
 root.render(
-	<React.StrictMode>
+	<ChakraProvider theme={theme}>
 		<App />
-	</React.StrictMode>
+	</ChakraProvider>
 )
