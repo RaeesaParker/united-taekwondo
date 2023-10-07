3524c7b3a6ebf0dc102da9d20bff2a9a323846b2
RaeesaParker <raeesaparker101@gmail.com>
2023-10-07 14:24:27 +0100

    Add intro

--- src/Components/Homepage/Introduction.tsx
+++ src/Components/Homepage/Introduction.tsx
@@ -1,15 +1,35 @@
-import { Box, Flex, Image } from "@chakra-ui/react"
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react"
 import React from "react"
 import imageJulie from "../../Assets/Julie.png";
 
 export const Introduction = () => {
   return (
-    <Flex>
-      <Box>
-
+    <Flex 
+      mt="10" 
+      p="5" 
+      bg="blue.dark"
+      justifyContent="space-evenly"
+      alignContent="center"
+      >
+      <Box w="30%">
+        <Image 
+          src={imageJulie} 
+          alt="Image of instructor Master Julie Graham" 
+          boxSize="25vw"
+          fallbackSrc='https://via.placeholder.com/150'
+          />
       </Box>
-      <Flex justifyContent="center">
-        <Image src={imageJulie} alt="Image of instructor Master Julie Graham" />
+      <Flex  
+        w="60%" 
+        direction="column" 
+        justifyContent="space-around"
+        alignContent="center"
+        >
+        <Text color="tones.white"> Welcome to United Taekwondo, where unity, discipline, and personal growth are at the core of our practice. Led by Master Julie Graham, a seasoned 6th Dan instructor with decades of experience, we embody the values of hard work and perseverance. At United Taekwondo, every member is valued and encouraged to reach their full potential. Join us in the journey of self-discovery, fitness, and camaraderie as we uphold the rich traditions of Taekwondo.</Text>
+        <Flex justifyContent="space-around" >
+          <Button bg="blue.light" color="tones.white" _hover={{color:"blue.dark", bg:"tones.white"}}>Find Out More</Button>
+          <Button bg="red.dark">  Train with Us</Button>
+        </Flex>
       </Flex>
     </Flex>
   )
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import { Box } from "@chakra-ui/react"
 import React from "react"
 import { NavBar } from "../Components/Common/Navbar"
+import { Introduction } from "../Components/Homepage/Introduction"
 
 export const Homepage = () => {
   return (
     <Box>
       <NavBar />
-
+      <Introduction />
     </Box>
   )
 }
