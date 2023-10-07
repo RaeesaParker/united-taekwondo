c597a46a004b6b2cba024889720c5d4cab863845
RaeesaParker <raeesaparker101@gmail.com>
2023-10-07 17:46:58 +0100

    Add address

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { NavBar } from "../Components/Common/Navbar"
 import { Introduction } from "../Components/Homepage/Introduction"
 import { ContactBar } from "../Components/Common/Contact/ContactBar"
+import { Address } from "../Components/Common/Contact/Address"
 
 export const Homepage = () => {
   return (
@@ -10,6 +11,7 @@ export const Homepage = () => {
       <NavBar />
       <Introduction />
       <ContactBar />
+      <Address />
     </Box>
   )
 }
